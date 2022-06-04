e1573d5e1cb5c12e9fa98243c96a88c77dad9daa
Maria Kovaleva <maria.kovaleva.blr@gmail.com>
2022-06-04 22:08:11 +0300

    project

---
+++ lessons/pr/js.js
@@ -0,0 +1,33 @@
+var cards = [
+    {
+        imgurl: "image/1.jpg",
+        title: "Фотоаппарат Sony",
+        rating: "4.8",
+        countReview: 35,
+        currentPrice: '5555 р',
+        oldPrice: '6666 р'
+    }
+    {
+        imgurl: "image/1.jpg",
+        title: "Фотоаппарат Sony",
+        rating: "4.8",
+        countReview: 35,
+        currentPrice: '5555 р',
+        oldPrice: '6666 р'
+    }
+    {
+        imgurl: "image/1.jpg",
+        title: "Фотоаппарат Sony",
+        rating: "4.8",
+        countReview: 35,
+        currentPrice: '5555 р',
+        oldPrice: '6666 р'
+    }
+]
+var ul = document.querySelector('.bloks ul')  <<-- - это чтобы достать элемент -->>
+
+var htmlstr = '';   достать строку
+
+for(var i=0; i<cards.length; i++){
+    htmlstr += 
+}
